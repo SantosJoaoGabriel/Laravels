cecd553897e9efe7f5467cc8694b50f6236fef12
Jaocomsono <joaopptopgg@gmail.com>
2025-08-26 20:20:09 -0300

    Feat:TelaAlulos

--- aulal1/routes/web.php
+++ aulal1/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlunoController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -12,4 +13,7 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::middleware('auth')->group(function () {
+    Route::resource('/alunos', AlunoController::class);
+});
 require __DIR__.'/auth.php';
